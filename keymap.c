fdabd34a9ae987969918a9016b752cb79259b82c
liuzhifei <1505994869@QQ.COM>
2022-09-26 10:34:03 +0800

    变更

--- keymap.c
+++ keymap.c
@@ -103,20 +103,20 @@ keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------------------------------------------------------------------------------------------------------------------------------|
  */
 [_LAYER0] = LAYOUT(
-        KC_CAPS, KC_F1  ,   KC_F2  , KC_F3,   KC_F4         , KC_F5  ,                        KC_F6,   KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_HOME, KC_END,
-        KC_GRV , KC_1   ,   KC_2   , KC_3 ,   KC_4          , KC_5   ,                        KC_6 ,   KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
-        KC_TAB , KC_Q   ,   KC_W   , KC_E ,   KC_R          , KC_T   ,                        KC_Y ,   KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        KC_ESC , KC_A   ,   KC_S   , KC_D ,   KC_F          , KC_G   , KC_BSPC,               KC_H ,   KC_J   , KC_K   , KC_L   , KC_BSPC, KC_QUOT, KC_ENT , KC_PGDN,
-        KC_LSFT, KC_Z   ,   KC_X   , KC_C ,   KC_V          , KC_B   , KC_ENT,                KC_N ,   KC_M   , LZF_7  , LZF_6  , KC_ENT , KC_RSFT, KC_UP  ,
-        KC_LCTL, KC_LALT,   KC_LGUI, MO(1),   LCTL_T(KC_F12), KC_LSFT, LT(2,KC_SPC), KC_RGUI, MO(4),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_CAPS       , KC_F1  ,   KC_F2  , KC_F3,   KC_F4         , KC_F5  ,                        KC_F6,   KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_HOME, KC_END,
+        KC_GRV        , KC_1   ,   KC_2   , KC_3 ,   KC_4          , KC_5   ,                        KC_6 ,   KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+        KC_TAB        , KC_Q   ,   KC_W   , KC_E ,   KC_R          , KC_T   ,                        KC_Y ,   KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+        KC_ESC        , KC_A   ,   KC_S   , KC_D ,   KC_F          , KC_G   , KC_BSPC,               KC_H ,   KC_J   , KC_K   , KC_L   , KC_BSPC, KC_QUOT, KC_ENT , KC_PGDN,
+        LSFT_T(KC_F12), KC_Z   ,   KC_X   , KC_C ,   KC_V          , KC_B   , KC_ENT,                KC_N ,   KC_M   , LZF_7  , LZF_6  , KC_ENT , KC_RSFT, KC_UP  ,
+        KC_LCTL       , KC_LALT,   KC_LGUI, MO(1),   KC_LCTL       , KC_LSFT, LT(2,KC_SPC), KC_RGUI, MO(4),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
 [_LAYER1] = LAYOUT(
         KC_TRNS, KC_TRNS  , KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS  , KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TAB , KC_GRV   , KC_QUOT, LZF_2  , LZF_3  ,        KC_F16 ,                        KC_NO  , KC_7   , KC_8   , KC_9   , KC_NO  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_ESC , KC_MINS  , KC_EQL , LZF_1  , LZF_5  ,        KC_BSLS, KC_BSPC,               KC_NO  , KC_4   , KC_5   , KC_6   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_LSFT, LZF_8    , LZF_9  , LZF_4  , KC_BSLS,        KC_F17 , KC_ENT,                KC_NO,   RGUI(KC_END),  RGUI(KC_HOME), KC_NO,   KC_TRNS, KC_RSFT, KC_UP,
+        KC_ESC , KC_MINS  , KC_EQL , LZF_1  , LZF_5  ,        KC_BSLS, KC_BSPC,               KC_PDOT, KC_4   , KC_5   , KC_6   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_LSFT, LZF_8    , LZF_9  , LZF_4  , KC_BSLS,        KC_F17 , KC_ENT ,               KC_PDOT, KC_1   , KC_2   , KC_3   , KC_TRNS, KC_TRNS, KC_VOLU,
         KC_LCTL, KC_LALT  , KC_LGUI, KC_TRNS, LCTL_T(KC_F12), KC_LSFT, KC_0   , KC_PDOT,      KC_TRNS, KC_TRNS, KC_F14 , KC_VOLD, KC_F15
     ),
 
@@ -125,7 +125,7 @@ keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS  , KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB , KC_GRV   , KC_QUOT, LZF_2  , LZF_3  ,        KC_F16 ,                        KC_NO,   KC_PGDN, KC_PGUP, KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         KC_ESC , KC_MINS  , KC_EQL , LZF_1  , LZF_5  ,        KC_BSLS, KC_BSPC,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_ENT,  KC_PGDN,
-        KC_LSFT, LZF_8    , LZF_9  , LZF_4  , KC_BSLS,        KC_F17 , KC_ENT ,               KC_NO  , KC_1   , KC_2   , KC_3   , KC_TRNS, KC_TRNS, KC_VOLU,
+        KC_LSFT, LZF_8    , LZF_9  , LZF_4  , KC_BSLS,        KC_F17 , KC_ENT,                KC_NO,   RGUI(KC_END),  RGUI(KC_HOME), KC_NO,   KC_TRNS, KC_RSFT, KC_UP,
         KC_LCTL, KC_LALT  , KC_LGUI, KC_TRNS, LCTL_T(KC_F12), KC_LSFT, KC_SPC, KC_RGUI,       KC_NO,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
